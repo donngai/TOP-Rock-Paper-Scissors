69c69d723e2c22f97b0fe7c230a976fd07dc8620
Donovan <donovanngai237@gmail.com>
2023-06-06 23:48:09 +0800

    Added code for the game in javascript

---
+++ script.js
@@ -0,0 +1,66 @@
+function getCompChoice () {
+    let choice = Math.floor(Math.random() * 3);
+
+    if (choice == 0) 
+        return "Rock";
+    else if (choice == 1)
+        return "Paper";
+    else
+        return "Scissors";
+}
+
+function getPlayerChoice () {
+    let input = prompt("Please enter 'Rock', 'Paper', or 'Scissors': ");
+
+    input = input.toLowerCase();
+    let cap = input.slice(0, 1);
+    cap = cap.toUpperCase();
+
+    return cap.concat(input.slice(1));
+}
+
+function playRound (playerChoice, compChoice) {
+    //Default value for playerChoice if incorrect input
+    if (playerChoice != "Rock" || playerChoice != "Paper" || playerChoice != "Scissors")
+        playerChoice = "Rock";
+
+    if (playerChoice === compChoice)
+        return 0;
+    else if ((playerChoice === "Rock" && compChoice === "Paper") || (playerChoice === "Paper" && compChoice === "Scissors") || (playerChoice === "Scissors" && compChoice === "Rock"))
+        return -1;
+    else
+        return 1;
+}
+
+function game () {
+    let playerPoint = 0;
+    let compPoint = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = getPlayerChoice();
+        let compChoice = getCompChoice();
+        
+        let roundWinner = playRound(playerChoice, compChoice);
+        if (roundWinner == 1) {
+            playerPoint++;
+            console.log("You won! " + playerChoice + " beats " + compChoice);
+        }
+        else if (roundWinner == -1) {
+            compPoint++;
+            console.log("You lose! " + compChoice + " beats " + playerChoice);
+        }
+        else
+            console.log("It's tied");
+
+        console.log(i);
+    }
+
+    if (playerPoint > compPoint)
+        console.log("You win the game!");
+    else if (compPoint > playerPoint)
+        console.log("The computer won the game");
+    else
+        console.log("It's tied!!!")
+}
+
+game();
